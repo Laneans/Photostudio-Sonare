24da5677d9326840592f22b2361961e85764e57c
Anastasia Domash <122899921+Laneans@users.noreply.github.com>
2023-01-22 20:14:38 +0300

    Update team.js

--- js/team.js
+++ js/team.js
@@ -15,7 +15,7 @@ window.onload = (function () {
     $menu.on('click', function () {
         //переход    
         $("body").fadeIn(470, "ease-in-out");
-        linkLocation = "MenuPC.html";
+        linkLocation = "html/menu.html";
         $("body").fadeOut(470, redirectPage);
         function redirectPage() {
             window.location = linkLocation;
@@ -27,7 +27,7 @@ window.onload = (function () {
     $error_page.on('click', function () {
         //переход    
         $("body").fadeIn(470, "ease-in-out");
-        linkLocation = "404_page.html";
+        linkLocation = "html/404_page.html";
         $("body").fadeOut(470, redirectPage);
         function redirectPage() {
             window.location = linkLocation;
