4a89324818e9d9d98b31501aa3900137e9eedf12
Anastasia Domash <122899921+Laneans@users.noreply.github.com>
2023-01-22 15:50:43 +0300

    Create services.js

---
+++ js/services.js
@@ -0,0 +1,31 @@
+window.onload = (function () {
+
+    //ЗАГРУЗКА СТРАНИЦЫ
+    var $body = $("body");
+    $(document).ready(function () {        
+        $body.fadeIn(1000, "swing");
+        $body.addClass('body_visible');
+
+    });
+
+
+    //ПЕРЕХОДЫ
+    //переход на страницу с меню
+    var $menu = $('#nav-open');
+    $menu.on('click', function () {
+        //переход    
+        $("body").fadeIn(470, "ease-in-out");
+        linkLocation = "MenuPC.html";
+        $("body").fadeOut(470, redirectPage);
+        function redirectPage() {
+            window.location = linkLocation;
+        }
+    });
+   
+
+    //переход вверх по стрелке из футера
+    var $to_up = $('#arr-up-link');
+    $to_up.click(function (event) {
+        $('body,html').animate({ scrollTop: 0 }, 2000, "swing");
+    });
+});
