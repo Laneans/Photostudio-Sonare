bb523665650814682914367d64c40a766532766a
Anastasia Domash <122899921+Laneans@users.noreply.github.com>
2023-01-22 20:10:33 +0300

    Update 404-page.js

--- js/404-page.js
+++ js/404-page.js
@@ -5,9 +5,7 @@ window.onload = (function () {
     $(document).ready(function () {        
         $body.fadeIn(1000, "swing");
         $body.addClass('body_visible');
-
     });
-
 
     //ПЕРЕХОДЫ
     //переход на страницу с меню
@@ -15,7 +13,7 @@ window.onload = (function () {
     $menu.on('click', function () {
         //переход    
         $("body").fadeIn(470, "ease-in-out");
-        linkLocation = "MenuPC.html";
+        linkLocation = "html/menu.html";
         $("body").fadeOut(470, redirectPage);
         function redirectPage() {
             window.location = linkLocation;
@@ -26,7 +24,7 @@ window.onload = (function () {
     var $to_main = $('#error-descr button');
     $to_main.on('click', function () {
         $("body").fadeIn(470, "ease-in-out");
-        linkLocation = "Main.html";
+        linkLocation = "html/main.html";
         $("body").fadeOut(470, redirectPage);
         function redirectPage() {
             window.location = linkLocation;
